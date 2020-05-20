a3179d2d962210b9d0f154036a8f3b41a02451bb
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-05-20 22:24:18 +0900

    good

--- src/controllers/user/UserViewController.js
+++ src/controllers/user/UserViewController.js
@@ -1,7 +1,17 @@
 class UserViewController{
     login=async(ctx,next)=>{
-        await ctx.render("login.html");
+        //如果是登陆状态就提示，您已成功登录，请直接访问首页
+        let data={isLogin:false}  //默认未登录
+        let userInfo=ctx.session.userInfo
+        if(userInfo){
+            data={
+                isLogin:true,
+                userName:userInfo.userName,
+            }
+        }
+        await ctx.render("login.html",data);
     }
+
     register=async(ctx,next)=>{
         await ctx.render("register.html");
     }
