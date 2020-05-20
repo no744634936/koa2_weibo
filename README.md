8278cd0b87dfd36f97062606f7ae313b65c0709b
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-05-20 10:50:31 +0900

    good

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 http://localhost:3000/register
 
 
+进行用户注册数据的验证
+查看 validator里的文件
 
-注册用户，加密并插入数据
+_validate.js
+validate_userInfo.js
+
+下载 ajv 用来验证json-schema :    npm install ajv --save
+
 
---
+++ src/validator/validate_userInfo.js
@@ -0,0 +1,61 @@
+/**
+ * @description 用户注册信息验证
+ * @author zhanghaifeng
+ * 
+ */
+
+let validate_data=require("../validator/_validate.js")
+
+
+ // 校验规则 json schema
+const SCHEMA = {
+    type: 'object',
+    properties: {
+        userName: {
+            type: 'string',
+            pattern: '^[a-zA-Z][a-zA-Z0-9_]+$', // 字母开头，字母数字下划线
+            maxLength: 255,
+            minLength: 2
+        },
+        password: {
+            type: 'string',
+            maxLength: 255,
+            minLength: 3
+        },
+        newPassword: {
+            type: 'string',
+            maxLength: 255,
+            minLength: 3
+        },
+        nickName: {
+            type: 'string',
+            maxLength: 255
+        },
+        picture: {
+            type: 'string',
+            maxLength: 255
+        },
+        city: {
+            type: 'string',
+            maxLength: 255,
+            minLength: 2
+        },
+        gender: {
+            type: 'number',
+            minimum: 1,
+            maximum: 3
+        }
+    }
+}
+
+
+//执行校验
+/**
+ * @description 校验用户数据
+ * @param {object} data 用户数据 
+ */
+validate_userInfo=(data={})=>{
+    return validate_data(SCHEMA,data)
+}
+
+module.exports=validate_userInfo
